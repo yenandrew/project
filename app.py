2c2834bda0b69a70accb61eb0c18605ff19d7eba
Andrew <Andrew.yen@yengeek.net>
2022-02-28 11:22:21 +0500

    Milestone 2 completed

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ with app.app_context():
     db.create_all()
 
 
+@app.route('/')
 @app.route('/login', methods=['GET', 'POST'])  # define login page path
 def login():  # define login page fucntion
     if request.method == 'GET':  # if the request is a GET we return the login page
@@ -93,42 +94,10 @@ def movie_rating_list():
     return render_template('ratings.html', **context)
 
 
-def get_data_from_tmdb(tmdb_id):
-    try:
-        tmdb_url = f'https://api.themoviedb.org/3/movie/{tmdb_id}?api_key={app.config.get("TMDB_APIKEY")}'
-        response = requests.get(tmdb_url)
-        if response.status_code == 200:
-            response_data = json.loads(response.content)
-            genre_list = [genre_dict['name'] for genre_dict in response_data['genres']]
-            return {'title': response_data['title'],
-                    'overview': response_data['overview'],
-                    'genre': ",".join(genre_list),
-                    'image_url': f'https://image.tmdb.org/t/p/original/'
-                                 f'{response_data["backdrop_path"]}'
-                    }
-    except ConnectionError:
-        logging.error('Error connecting with TMDB!')
-
-
 @app.route('/profile')  # profile page that return 'profile'
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.username)
-
-
-@app.route('/')
-def home():
-    """
-    This route will display the movie detail
-    :return:
-    """
-    tmdb_id_list = ['24428', '141052', '157336']
-    tmdb_id = random.choice(tmdb_id_list)
-    movie_data = get_data_from_tmdb(tmdb_id)
-    movie_data['wikipedia_url'] = wikipedia.page(title=f"{movie_data['title']}(film)", auto_suggest=True,
-                                                 redirect=True, preload=False)
-
-    return render_template('index.html', **movie_data)
 
 
 if __name__ == '__main__':
