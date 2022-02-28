07e4f2f46a38adfb2ec47748e7645514fd56054c
Andrew <Andrew.yen@yengeek.net>
2022-02-28 09:21:40 +0500

    Add submit rating feature

--- config.py
+++ config.py
@@ -7,4 +7,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     TMDB_APIKEY = os.getenv('TMDB_APIKEY')
-
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY')
