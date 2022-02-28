f675715c4981a282893804783e566a7fb89e83d3
Andrew <Andrew.yen@yengeek.net>
2022-02-28 09:20:46 +0500

    Add Movie Rating model in model.py

--- model.py
+++ model.py
@@ -8,3 +8,12 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
     username = db.Column(db.String(1000), unique=True)
+    movie_ratings = db.relationship("MovieRating")
+
+
+class MovieRating(db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # primary keys are required by SQLAlchemy
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    rating = db.Column(db.Integer)
+    comment = db.Column(db.String(1000))
+    movie_id = db.Column(db.Integer)
