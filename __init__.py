66fa15cb032f010d4febfa3c64fa10f748fd2df0
adnanghaffardash07 <testadnandash07@gmail.com>
2022-03-01 10:20:36 +0500

    add gunicorn

--- __init__.py
+++ __init__.py
@@ -4,26 +4,3 @@ from extension import login_manager, db
 from flask_sqlalchemy import SQLAlchemy
 
 
-def create_app(config_class):
-    app = Flask(__name__)
-    app.config.from_object(config_class)
-
-    from model import User
-    @login_manager.user_loader
-    def load_user(user_id):  # reload user object from the user ID stored in the session
-        # since the user_id is just the primary key of our user table, use it in the query for the user
-        return User.query.get(int(user_id))
-
-    extension(app)
-    return app
-
-
-def extension(app):
-    """
-    Register 0 or more extensions (mutates the app passed in).
-    :param app: Flask application instance
-    :return:
-    """
-    db.init_app(app)
-    login_manager.init_app(app)
-    login_manager.login_view = 'login'
--- app.py
+++ app.py
@@ -1,18 +1,36 @@
-import json
-import logging
-import random
-from . import create_app
-import requests
-from requests.exceptions import ConnectionError
-import wikipedia
-
 from config import Config
-from flask import url_for, request, render_template, flash
+from flask import url_for, request, render_template, flash, Flask
 from flask_login import logout_user, login_required, login_user, current_user
 from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.utils import redirect
-from extension import db
+from extension import db, login_manager
 from model import User, MovieRating
+
+
+def create_app(config_class):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    from model import User
+    @login_manager.user_loader
+    def load_user(user_id):  # reload user object from the user ID stored in the session
+        # since the user_id is just the primary key of our user table, use it in the query for the user
+        return User.query.get(int(user_id))
+
+    extension(app)
+    return app
+
+
+def extension(app):
+    """
+    Register 0 or more extensions (mutates the app passed in).
+    :param app: Flask application instance
+    :return:
+    """
+    db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
+
 
 app = create_app(Config)
 with app.app_context():
